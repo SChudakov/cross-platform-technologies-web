1c067a634ea8d9aaa1c1fdfa28e015151500a8d6
Semen Chudakov <semen.chudakov7@gmail.com>
2020-10-20 18:05:56 +0300

    Project setup

---
+++ src/main/java/com/chudakov/crossplatformtechnologiesweb/CrossPlatformTechnologiesWebApplication.java
@@ -0,0 +1,13 @@
+package com.chudakov.crossplatformtechnologiesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformTechnologiesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPlatformTechnologiesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chudakov/crossplatformtechnologiesweb/CrossPlatformTechnologiesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chudakov.crossplatformtechnologiesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatformTechnologiesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
